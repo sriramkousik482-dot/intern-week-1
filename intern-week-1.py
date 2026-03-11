35f10480ce6020034531ff82602200fd9d2e55e0
sriramkousik482-dot <sriramkousik482@gmail.com>
2026-03-11 16:48:24 +0530

    Add files via upload

---
+++ intern-week-1.py
@@ -0,0 +1,11 @@
+print("hello world")
+
+
+name=input("enter your name? ")
+age=int(input("enter your age "))
+hobby=input("enter your hobby? ")
+
+
+print("\n🎉 Welcome", name + "! 🎉")
+print(f"your age is {age} and love {hobby}")
+
