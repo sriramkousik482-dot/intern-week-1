2cf8f66cd201a26fa5e20b66bca8337b5d498eef
sriramkousik482-dot <sriramkousik482@gmail.com>
2026-03-14 09:53:51 +0530

    Add files via upload

--- intern-week-1.py
+++ intern-week-1.py
@@ -5,7 +5,9 @@ name=input("enter your name? ")
 age=int(input("enter your age "))
 hobby=input("enter your hobby? ")
 
-
 print("\n🎉 Welcome", name + "! 🎉")
 print(f"your age is {age} and love {hobby}")
 
+x=[10,20,30]
+x.append((40,50))
+print(len(x))
---
+++ personal_intro.py
@@ -0,0 +1,27 @@
+Personal Introduction Python Program
+📌 Project Overview
+
+This project is a simple Python program that collects user information and displays a personalized welcome message.
+
+🚀 Features
+
+Takes user input (name, age, hobby)
+
+Uses Python variables and input() function
+
+Displays formatted output using f-strings
+
+Beginner-friendly Python program
+
+🛠 Technologies Used
+
+Python 3
+
+📊 Sample Output
+
+What is your name? Alex
+How old are you? 21
+What is your favorite hobby? Coding
+
+🎉 Welcome Alex! 🎉
+You are 21 years old and love Coding.
